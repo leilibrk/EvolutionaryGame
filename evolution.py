04034f0d615e45d5b2948791268a90afe91405bf
Leili Barekatain <61875691+leilibrk@users.noreply.github.com>
2022-06-28 21:44:51 +0430

    implement generate_new_population

--- evolution.py
+++ evolution.py
@@ -22,8 +22,10 @@ class Evolution:
         players = self.roulette_wheel(players, num_players)
         # TODO (Additional: Implement SUS here)
         players = self.SUS(players, num_players)
-        # TODO (Additional: Learning curve)
+        # Q-tournament
         players = self.Q_tournament(players, num_players)
+        # TODO (Additional: Learning curve)
+
         return players[: num_players]
 
     def generate_new_population(self, num_players, prev_players=None):
@@ -39,8 +41,18 @@ class Evolution:
             return [Player(self.game_mode) for _ in range(num_players)]
         else:
             # TODO ( Parent selection and child generation )
-            new_players = prev_players  # DELETE THIS AFTER YOUR IMPLEMENTATION
-            return new_players
+            parents = self.roulette_wheel(prev_players, num_players)
+            children = []
+            for p in parents:
+                for q in parents:
+                    if p == q:
+                        continue
+                    child1, child2 = self.crossover(p, q)
+                    child1_mut = self.mutate(child1, 0.5)
+                    child2_mut = self.mutate(child2, 0.5)
+                    children.append(child1_mut)
+                    children.append(child2_mut)
+            return children
 
     def clone_player(self, player):
         """
@@ -88,3 +100,33 @@ class Evolution:
             q_random_players = np.random.choice(players, q)
             next_pop.append(max(q_random_players, key=lambda player: player.fitness))
         return next_pop
+
+    def crossover(self, p, q):
+        child1 = self.clone_player(p)
+        child2 = self.clone_player(q)
+        child1.nn.w1, child2.nn.w1 = self.cross(p.nn.w1, q.nn.w1)
+        child1.nn.b1, child2.nn.b1 = self.cross(p.nn.b1, q.nn.b1)
+        child1.nn.w2, child2.nn.w2 = self.cross(p.nn.w2, q.nn.w2)
+        child1.nn.b2, child2.nn.b2 = self.cross(p.nn.b2, q.nn.b2)
+        return child1, child2
+
+    def mutate(self, child, thresh):
+        prob = 0.25
+        r = np.random.uniform(0, 1)
+        if r >= prob:
+            return child
+        child.nn.w1 += np.random.normal(0, thresh, child.nn.w1.shape)
+        child.nn.w2 += np.random.normal(0, thresh, child.nn.w2.shape)
+        child.nn.b1 += np.random.normal(0, thresh, child.nn.b1.shape)
+        child.nn.b2 += np.random.normal(0, thresh, child.nn.b2.shape)
+        return child
+
+    def cross(self, a, b):
+        prob = 0.8
+        r = np.random.uniform(0, 1)
+        mid = int(a.shape[0] / 2)
+        if r >= prob:
+            return a, b
+        ch1 = np.concatenate((a[:mid], b[mid:]), axis=0)
+        ch2 = np.concatenate((b[:mid], a[mid:]), axis=0)
+        return ch1, ch2
